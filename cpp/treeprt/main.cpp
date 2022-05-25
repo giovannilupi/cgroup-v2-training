e63f0a71294069133790e3d10f95889bc5aa1deb
Giovanni Lupi <giovanniedoardo.lupi@mail.polimi.it>
2022-05-25 18:54:47 +0200

    Work in progress

--- cpp/treeprt/main.cpp
+++ cpp/treeprt/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <cstring>
 #include <cerrno>
@@ -7,70 +8,109 @@
 
 using namespace std;
 
-static void ProcessDirectory(string aDirName, int aLevel = 0);
-static void PrintDirectory(string aPathName, string aDirName, int aLevel);
-static string GetFileContent(string aDirName, string aFileName);
-static void Indent(int aLevel);
+static void ProcessDirectory(string aBaseName, string aDirName, int aLevel = 0);
+static void PrintDirectory(string aBaseName, string aDirName, int aLevel);
+static string GetFileContent(string aBaseName, string aFileName);
+static string Indent(int aLevel);
+static string TypeTrim(string type);
+static string MakePath(string aBase, string aLast);
+
 
 int main(int argc, char *argv[])
 {
-    cout << argv[1] << endl;
-    ProcessDirectory(argv[1], 1);
+    if (argc < 2)
+        return 1;
+
+    string path = argv[1];
+
+    size_t posSep = path.find_last_of("/");
+    string baseName = path.substr(0, posSep);
+    string dirName = path.substr(posSep+1);
+
+    ProcessDirectory(baseName, dirName, 1);
     return 0;
 }
 
-static void ProcessDirectory(string aDirName, int aLevel)
+static void ProcessDirectory(string aBaseName, string aDirName, int aLevel)
 {
     DIR *dir;
     struct dirent *dp;
 
-    dir = opendir(aDirName.c_str());
+    string path = MakePath(aBaseName, aDirName);
+    dir = opendir(path.c_str());
     if (!dir)
         return;
 
-    while (true) {
-        dp = readdir(dir);
-        if (dp == nullptr)
-            break;
-        if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
-            continue;
-        if (dp->d_name[0] == '.')
-            continue;
-        if (dp->d_type != DT_DIR)
-            continue;
+    PrintDirectory(aBaseName, aDirName, aLevel);
 
-        PrintDirectory(aDirName, dp->d_name, aLevel);
-        string newDirName = aDirName + "/" + dp->d_name;
-        ProcessDirectory(newDirName, aLevel + 1);
+    while ((dp = readdir(dir))) {
+        // skip all files and all folders whose name starts with a dot
+        if (dp->d_name[0] == '.' || dp->d_type != DT_DIR)
+            continue;
+        ProcessDirectory(aBaseName, dp->d_name, aLevel + 1);
     }
     closedir(dir);
 }
 
-static void PrintDirectory(string aPathName, string aDirName, int aLevel)
+static void PrintDirectory(string aBaseName, string aDirName, int aLevel)
 {
-    string fullName = aPathName + "/" + aDirName;
+    string fullName = MakePath(aBaseName, aDirName);
     string cgtype = GetFileContent(fullName, "cgroup.type");
     string subtreeCtrl = GetFileContent(fullName, "cgroup.subtree_control");
-    Indent(aLevel);
-    cout << aDirName << "  [" << cgtype << "] (" << subtreeCtrl << ")" << endl;
+
+    cout << Indent(aLevel) << aDirName << "  [" << TypeTrim(cgtype) << "]";
+    if (!subtreeCtrl.empty())
+        cout << " (" << subtreeCtrl << ")";
+    cout << endl;
+    if (cgtype == "domain" || cgtype == "domain threaded") {
+        string fContent = GetFileContent(fullName, "cgroup.procs");
+        if (!fContent.empty())
+            cout << Indent(aLevel) << "PIDS: {" << fContent << "}" << endl;
+    } else if (cgtype == "threaded") {
+        string fContent = GetFileContent(fullName, "cgroup.threads");
+        if (!fContent.empty())
+        cout << Indent(aLevel) << "TIDS: {" << fContent << "}" << endl;
+    }
 }
 
-static string GetFileContent(string aDirName, string aFileName)
+static string GetFileContent(string aBaseName, string aFileName)
 {
-    string pathName = aDirName + "/" + aFileName;
-    //cout << ">>>>> opening " << pathName << endl;
+    string pathName = MakePath(aBaseName, aFileName);
+
     ifstream is(pathName);
     string s;
     if (is.is_open()) {
-        is >> s;
+        string s2;
+        while (is >> s2) {
+            if (s.size() > 0)
+                s.append(" ");
+            s.append(s2);
+        }
         is.close();
     }
     return s;
 }
 
-static void Indent(int aLevel)
+static string Indent(int aLevel)
 {
+    ostringstream os;
     for (int i = 0; i < aLevel; ++i)
-        cout << "    ";
+        os << "    ";
+    return os.str();
 }
 
+static string TypeTrim(string type)
+{
+    return (type == "domain")
+                ? "d"
+                : (type == "threaded")
+                        ? "t"
+                        : (type == "domain threaded")
+                            ? "dt"
+                            : "di";
+}
+
+static string MakePath(string aBase, string aLast)
+{
+    return aBase + "/" + aLast;
+}
